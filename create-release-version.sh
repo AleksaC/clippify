b34959d90c590d75b666bd10115756e08ca50dca
AleksaC <aleksacukovic1@gmail.com>
2020-03-03 22:38:22 +0100

    Add icon128.png and update manifest.json and create-release-version.sh

--- create-release-version.sh
+++ create-release-version.sh
@@ -1,5 +1,5 @@
 mkdir -p release
 cp -r src release/
+cp icons/icon128.png release/
 sed -e '/\"background\":/d' manifest.json > release/manifest.json
 cd release && zip -r ../release.zip ./* && cd ..
-ls
