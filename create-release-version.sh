c537dcbf3cf2172e68e130203b7b644d38207eb3
AleksaC <aleksacukovic1@gmail.com>
2025-05-05 20:36:30 +0200

    upgrade extension to v3 manifest

--- create-release-version.sh
+++ create-release-version.sh
@@ -1,5 +1,8 @@
-mkdir -p release
-cp -r src release/
-cp -r icons/ release/
-sed -e '/\"background\":/d' manifest.json > release/manifest.json
-cd release && zip -r ../release.zip ./* && cd ..
+#!/bin/bash
+
+set -eoux pipefail
+
+rm -rf release
+mkdir release
+cp -r src icons manifest.json release/
+cd release && zip -r ../release.zip ./*
